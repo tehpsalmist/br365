352e8424faecd8cc3cbc0c2bf0b3411791243d78
tehpsalmist <bstewardcodes@gmail.com>
2022-07-29 02:18:30 -0400

    todays plan is...

--- api/_controllers/functions/sendTextReminder.ts
+++ api/_controllers/functions/sendTextReminder.ts
@@ -1,15 +1,15 @@
-import { reduceReferenceStringToObject } from '../utilities'
-import { sendText } from './sendEmail'
-import { baseUrl } from '../../_config'
+import { reduceReferenceStringToObject } from "../utilities";
+import { sendText } from "./sendEmail";
+import { baseUrl } from "../../_config";
 
 export default (reading, plan) => {
-  const { book, chapter } = reduceReferenceStringToObject(reading[0])
+  const { book, chapter } = reduceReferenceStringToObject(reading[0]);
 
-  const url = `${baseUrl}/read/${plan.translation}/${book}${chapter}`
+  const url = `${baseUrl}/read/${plan.translation}/${book}${chapter}`;
 
   return sendText({
     to: `${plan.phone}@${plan.carrier}`,
     subject: plan.planName,
-    text: url
-  }).catch((err) => console.error('text error:', err))
-}
+    text: `Today's reading is: ${url}`,
+  }).catch((err) => console.error("text error:", err));
+};
